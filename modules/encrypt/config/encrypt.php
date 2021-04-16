7782fe892fbec7f468741acd44586b7094e20cf0
Tom van Overloop <tom@xtradigital>
2021-04-16 14:29:43 +0200

    took encrypt module from devel and removed mcrypt

--- modules/encrypt/config/encrypt.php
+++ modules/encrypt/config/encrypt.php
@@ -1,18 +1,18 @@
 <?php
-
+/**
+ * The following options must be set:
+ * string   type    Driver Type
+ * string   key     Secret Passphrase
+ */
 return [
-
 	'default' => [
-		'type' => 'mcrypt',
-		/**
-		 * The following options must be set:
-		 *
-		 * string   key     secret passphrase
-		 * integer  mode    encryption mode, one of MCRYPT_MODE_*
-		 * integer  cipher  encryption cipher, one of the Mcrpyt cipher constants
-		 */
-		'cipher' => MCRYPT_RIJNDAEL_128,
-		'mode' => MCRYPT_MODE_NOFB,
+		'type'	 => 'openssl',
+		'key'	 => NULL,
+		'cipher' => Encrypt_Engine_Openssl::AES_256_CBC
 	],
-
+//	  'sodium' => [
+//		'type'    => 'sodium',
+//		'key'     => NULL,
+//		'cipher'  => Encrypt_Engine_Sodium::AES_256_GCM
+//    ]
 ];
